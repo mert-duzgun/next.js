683933115176e40c6e41c031a1ef4be7eb39d4b4
Zack Tanner <1939140+ztanner@users.noreply.github.com>
2024-07-03 16:08:16 -0700

    Revert "add layerAssets property to FlightDataPath (#66354)"
    This reverts commit 312eb61d24beef63cddf955a1324f2f6523100fa.

--- test/development/acceptance-app/hydration-error.test.ts
+++ test/development/acceptance-app/hydration-error.test.ts
@@ -818,20 +818,21 @@ describe('Error overlay for hydration errors', () => {
     if (isTurbopack) {
       expect(fullPseudoHtml).toMatchInlineSnapshot(`
         "...
-          <RedirectBoundary>
-            <RedirectErrorBoundary router={{...}}>
-              <InnerLayoutRouter parallelRouterKey="children" url="/" tree={[...]} childNodes={Map} segmentPath={[...]} ...>
-                <ClientPageRoot props={{params:{}, ...}} Component={function Page}>
-                  <Page params={{}} searchParams={{}}>
-                    <div>
+          <NotFoundErrorBoundary pathname="/" notFound={[...]} notFoundStyles={[...]} asNotFound={undefined} missingSlots={Set}>
+            <RedirectBoundary>
+              <RedirectErrorBoundary router={{...}}>
+                <InnerLayoutRouter parallelRouterKey="children" url="/" tree={[...]} childNodes={Map} segmentPath={[...]} ...>
+                  <ClientPageRoot props={{params:{}, ...}} Component={function Page}>
+                    <Page params={{}} searchParams={{}}>
                       <div>
                         <div>
                           <div>
-                            <Mismatch>
-                              <p>
-                                <span>
-        +                          client
-        -                          server"
+                            <div>
+                              <Mismatch>
+                                <p>
+                                  <span>
+        +                            client
+        -                            server"
       `)
     } else {
       expect(fullPseudoHtml).toMatchInlineSnapshot(`
